c536be0d3fe228b278dba801342ff477f2ab3645
Stevanus Felixiano <stevanusf77@gmail.com>
2026-09-14 09:20:38 +0800

    Initial Commit

---
+++ iEat/iEatApp.swift
@@ -0,0 +1,17 @@
+//
+//  iEatApp.swift
+//  iEat
+//
+//  Created by Stevanus Felixiano on 14/09/26.
+//
+
+import SwiftUI
+
+@main
+struct iEatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
